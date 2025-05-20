4d7aca61e7b35fb5acf08dc37efe4f3600826df5
Muhammad Ashir Mustafa <muhammadashirone@gmail.com>
2025-05-20 07:07:42 +0500

    updated 29.py

--- leetcode/29.py
+++ leetcode/29.py
@@ -1,24 +1,38 @@
-def divide(dividend: int, divisor: int) -> int:
-    INT_MIN = -2**31
-    INT_MAX = 2**31 - 1
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        INT_MIN = -2147483648
+        INT_MAX = abs(INT_MIN) - 1
+        
+        quotient = 0
+        
+        is_negative = False
+        if (dividend < 0) != (divisor < 0):
+            is_negative = True
 
+            
+        a = abs(dividend)
+        b = abs(divisor)
+        
+        for i in range(31, -1, -1):
+            if ( a >> i) >= b:
+                quotient = quotient + (1 << i)
+                a = a - (b << i)
+                
+        if is_negative == True:
+            quotient = -quotient
+                
+        if quotient < INT_MIN:
+            return INT_MIN
+        if quotient > INT_MAX:
+            return INT_MAX   
+        
+        return quotient     
+        
 
-    if dividend == INT_MIN and divisor == -1:
-        return INT_MAX
-
-    is_negative = (dividend < 0) != (divisor < 0)
-
-
-    a = abs(dividend)
-    b = abs(divisor)
-    result = 0
-
-    for i in range(31, -1, -1):
-        if (a >> i) >= b:
-            result += 1 << i
-            a -= b << i
-
-    if is_negative:
-        result = -result
-
-    return result
+                
+        
+sol = Solution()
+# sol.divide(10,3)
+print(sol.divide(-2147483648,1))
+# print(sol.divide(7,-3))
+        
