a2f306e78743f0c4745fe0fdce09f29839806c23
Muhammad Ashir Mustafa <muhammadashirone@gmail.com>
2025-05-25 14:07:48 +0500

    Refactor merge function into Solution class and update example usage

--- leetcode/Easy/88.py
+++ leetcode/Easy/88.py
@@ -1,21 +1,24 @@
 from typing import List
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-    """
-    Do not return anything, modify nums1 in-place instead.
-    
-    Merge nums2 into nums1 in non-decreasing order.
-    nums1 has enough space (size that is equal to m + n) to hold additional elements from nums2.
-    The first m elements of nums1 are valid; the rest are zeros as placeholders.
-    """
-    # Write your code here
-    pass
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p = 0
+        for i in range(m, m + n):
+            if p > n - 1:
+                p = p + 1
+                break
+            else:
+                nums1[i] = nums2[p]
+                p += 1
+        nums1.sort()
+        pass
 
-# Example usage
+
 nums1 = [1, 2, 3, 0, 0, 0]
 m = 3
 nums2 = [2, 5, 6]
 n = 3
 
-merge(nums1, m, nums2, n)
-print(nums1)  # Expected output: [1, 2, 2, 3, 5, 6]
+sol = Solution()
+sol.merge(nums1, m, nums2, n)
+print(nums1)  
